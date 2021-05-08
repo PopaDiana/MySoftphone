f42c82b8d4cd5e2fe61abe69f8dc66be93248a6a
PopaDiana <didi.andreea.dp@gmail.com>
2021-05-09 00:59:39 +0300

    first UI commit

---
+++ MySoftphone/MySoftphone/ObservableObject.cs
@@ -0,0 +1,16 @@
+﻿using Ozeki.VoIP;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MySoftphone.UI.ViewModel
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string propName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
---
+++ MySoftphone/MySoftphone/UI/ViewModel/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySoftphone.UI.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public MainViewModel()
+        {
+
+        }
+    }
+}
