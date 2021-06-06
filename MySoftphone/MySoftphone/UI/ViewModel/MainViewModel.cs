8ddea9913925e6200b75327e400a84d3944178e2
PopaDiana <didi.andreea.dp@gmail.com>
2021-06-06 21:27:40 +0300

    more UI

--- MySoftphone/MySoftphone/UI/ViewModel/MainViewModel.cs
+++ MySoftphone/MySoftphone/UI/ViewModel/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySoftphone.MVVM;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reactive.Linq;
@@ -9,8 +10,43 @@ namespace MySoftphone.UI.ViewModel
 {
     class MainViewModel : ObservableObject
     {
+        private object currentView;
+
+        public SipRegistrationViewModel SipRegVM { get; set; }
+
+        public AudioCallViewModel AudioCallVM { get; set; }
+
+        public object CurrentView
+        {
+            get { return currentView; }
+            set 
+            { 
+                currentView = value;
+                OnPropertyChanged();
+            } 
+        }
+
+        public RelayCommand SipRegViewCommand { get; set; }
+
+        public RelayCommand AudioCallViewCommand { get; set; }
+
+        public RelayCommand DragWindow { get; set; }
+
         public MainViewModel()
         {
+            SipRegVM = new SipRegistrationViewModel();
+            AudioCallVM = new AudioCallViewModel();
+            CurrentView = SipRegVM;
+
+            SipRegViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = SipRegVM;
+            });
+
+            AudioCallViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = AudioCallVM;
+            });
 
         }
     }
